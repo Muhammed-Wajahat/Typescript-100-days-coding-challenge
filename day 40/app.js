c9462866d80a9822bcc7c23194722b92456a53b5
Wajahat Ali <wajahatalisadique@gmail.com>
2024-04-10 14:22:40 -0700

    add new file

---
+++ day 40/app.js
@@ -0,0 +1,26 @@
+"use strict";
+//🚀 Day 40 Challenge: Start Coding! 🚀
+//Question 1
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//Question 2
+let counter = 10;
+while (counter > 0) {
+    if (counter === 5) {
+        break;
+    }
+    console.log("Hello World");
+    counter--;
+}
+//Question 3
+function getStr(str) {
+    const vowels = "aeiouAEIOU";
+    for (const char of str) {
+        if (vowels.includes(char)) {
+            console.log(`first vowel found: "${char}"`);
+            break;
+        }
+    }
+}
+getStr("Wajahat Ali");
